4a687a3c7ef91f783fc79aebe914c4d1e7d38f79
Jineet <61728875+vorajineet@users.noreply.github.com>
2020-10-12 23:25:15 +0530

    Update FileReaderServiceImpl.java

--- ProcessSpreadSheet - AEM6.5/core/src/main/java/org/corteva/aem/calc/core/services/impl/FileReaderServiceImpl.java
+++ ProcessSpreadSheet - AEM6.5/core/src/main/java/org/corteva/aem/calc/core/services/impl/FileReaderServiceImpl.java
@@ -2,6 +2,7 @@ package org.corteva.aem.calc.core.services.impl;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Objects;
 
 import javax.jcr.Node;
 import javax.jcr.RepositoryException;
@@ -37,11 +38,13 @@ public class FileReaderServiceImpl implements FileReaderService {
 			// Creating a workbook from the excel file
 			XSSFWorkbook workbook = new XSSFWorkbook(new File(filePath));
 
+			//workbook.setMissingCellPolicy(MissingCellPolicy.RETURN_BLANK_AS_NULL);
+
 			// Getting the number of sheets
 			int numberOfSheets = workbook.getNumberOfSheets();
 
 			for (int i = 0; i < numberOfSheets; i++) {
-				
+
 				// Getting the sheet at the ith position
 				XSSFSheet sheet = workbook.getSheetAt(i);
 
@@ -49,48 +52,160 @@ public class FileReaderServiceImpl implements FileReaderService {
 
 				if (sheet.getSheetName().equals(AppConstants.SHEET_SELECT_SAVINGS)) {
 					
+					log.info("Inside Select Savings Sheet");
+
 					int tradeCode = 0, productColumnIndex = 1, progromCodeColumnIndex = 2, soldAsUOM = 3,
 							selectSavingsEarningRate = 4, requriesPrepay = 5;
 
 					for (Row myrow : sheet) {
-						
-						//Skip 1st Row
+
+						// Skip 1st Row
 						if (myrow.getRowNum() != 0) {
-							
+
 							for (Cell cell : myrow) {
 
+								String nodePath = AppConstants.INITIAL_STRUCTURE + "select-savings/";
+								String programCode = "", product = "";
+
 								cell = myrow.getCell(progromCodeColumnIndex);
-								String programCode = readCellData(cell);
-								log.info("programCode: " + programCode);
-								createNode(AppConstants.INITIAL_STRUCTURE, session, programCode);
+
+								if (Objects.nonNull(cell)) {
+									programCode = readCellData(cell);
+									log.info("programCode: " + programCode);
+									createNode(nodePath, session, programCode);
+								}
+
 								cell = myrow.getCell(productColumnIndex);
-								String product = readCellData(cell);
-								log.info("product: " + product);
-								createNode(AppConstants.INITIAL_STRUCTURE + programCode, session, product);
+
+								if (Objects.nonNull(cell)) {
+									product = readCellData(cell);
+									log.info("product: " + product);
+									createNode(nodePath + programCode, session, product);
+								}
+
 								cell = myrow.getCell(requriesPrepay);
-								String prePay = readCellData(cell);
-								createProperties(AppConstants.INITIAL_STRUCTURE + programCode + "/" + product, session, AppConstants.SELECT_SAVINGS_REQUIRES_PREPAY, prePay);
+
+								if (Objects.nonNull(cell)) {
+									String prePay = readCellData(cell);
+									createProperties(nodePath + programCode + "/" + product, session,
+											AppConstants.SELECT_SAVINGS_REQUIRES_PREPAY, prePay);
+								}
+
 								cell = myrow.getCell(selectSavingsEarningRate);
-								String earningsRate = readCellData(cell);
-								createProperties(AppConstants.INITIAL_STRUCTURE + programCode + "/" + product, session, AppConstants.SELECT_SAVINGS_EARNINGS_RATE, earningsRate);
+
+								if (Objects.nonNull(cell)) {
+									String earningsRate = readCellData(cell);
+									createProperties(nodePath + programCode + "/" + product, session,
+											AppConstants.SELECT_SAVINGS_EARNINGS_RATE, earningsRate);
+								}
+
 								cell = myrow.getCell(tradeCode);
-								String tradeCodeValue = readCellData(cell);
-								createProperties(AppConstants.INITIAL_STRUCTURE + programCode + "/" + product, session, AppConstants.SELECT_SAVINGS_TRADE_CODE, tradeCodeValue);
+
+								if (Objects.nonNull(cell)) {
+									String tradeCodeValue = readCellData(cell);
+									createProperties(nodePath + programCode + "/" + product, session,
+											AppConstants.SELECT_SAVINGS_TRADE_CODE, tradeCodeValue);
+								}
+
 								cell = myrow.getCell(soldAsUOM);
-								String soldAsUOMValue = readCellData(cell);
-								createProperties(AppConstants.INITIAL_STRUCTURE + programCode + "/" + product, session, AppConstants.SELECT_SAVINGS_SOLD_AS_UOM, soldAsUOMValue);
-								
+
+								if (Objects.nonNull(cell)) {
+									String soldAsUOMValue = readCellData(cell);
+									createProperties(nodePath + programCode + "/" + product, session,
+											AppConstants.SELECT_SAVINGS_SOLD_AS_UOM, soldAsUOMValue);
+								}
 
 							}
 
 						}
 					}
 				}
+
+				if (sheet.getSheetName().equals(AppConstants.SHEET_ENLIST_AHEAD)) {
+					
+					log.info("Inside Enlist Ahead Sheet");
+
+					int product = 0, programCode = 1, programMatchRate = 2, applicationUOM = 3, soldAsUOM = 4,
+							coversionFactor = 5, payRate = 6;
+
+					for (Row myrow : sheet) {
+
+						// Skip 1st Row
+						if (myrow.getRowNum() != 0) {
+
+							for (Cell cell : myrow) {
+
+								String nodePath = AppConstants.INITIAL_STRUCTURE + "enlist-ahead/";
+								String programCodeValue = "", productValue = "";
+								
+								cell = myrow.getCell(programCode);
+								if (Objects.nonNull(cell)) {
+									programCodeValue = readCellData(cell);
+									log.info("programCode: " + programCodeValue);
+									createNode(nodePath, session, programCodeValue);
+								}
+								
+								cell = myrow.getCell(product);
+								
+								if (Objects.nonNull(cell)) {
+									productValue = readCellData(cell);
+									log.info("product: " + productValue);
+									createNode(nodePath + programCodeValue, session, productValue);
+								}
+								
+								cell = myrow.getCell(programMatchRate);
+								
+								if (Objects.nonNull(cell)) {
+									String programMatchRateValue = readCellData(cell);
+									createProperties(nodePath + programCodeValue + "/" + productValue, session,
+											AppConstants.ENLIST_AHEAD_PROGRAM_MATCH_RATE, programMatchRateValue);
+								}
+								
+								cell = myrow.getCell(applicationUOM);
+								
+								if (Objects.nonNull(cell)) {
+									String applicationUOMValue = readCellData(cell);
+									createProperties(nodePath + programCodeValue + "/" + productValue, session,
+											AppConstants.ENLIST_AHEAD_APPLICATION_UOM, applicationUOMValue);
+								}
+
+								cell = myrow.getCell(soldAsUOM);
+
+								if (Objects.nonNull(cell)) {
+									String soldAsUOMValue = readCellData(cell);
+									createProperties(nodePath + programCodeValue + "/" + productValue, session,
+											AppConstants.ENLIST_AHEAD_SOLD_AS_UOM, soldAsUOMValue);
+								}
+
+								cell = myrow.getCell(coversionFactor);
+
+								if (Objects.nonNull(cell)) {
+									String coversionFactorValue = readCellData(cell);
+									createProperties(nodePath + programCodeValue + "/" + productValue, session,
+											AppConstants.ENLIST_AHEAD_CONVERSION_FACTOR, coversionFactorValue);
+								}
+
+								cell = myrow.getCell(payRate);
+
+								if (Objects.nonNull(cell)) {
+									String payRateValue = readCellData(cell);
+									createProperties(nodePath + programCodeValue + "/" + productValue, session,
+											AppConstants.ENLIST_AHEAD_PAY_RATE, payRateValue);
+								}
+
+							}
+
+						}
+					}
+
+				}
 			}
 			workbook.close();
 
 		} catch (EncryptedDocumentException | InvalidFormatException | IOException e) {
-			log.error(e.getMessage(), e);
+			log.error("Error Occured in ReadExcel: " + e.getMessage());
+		} catch (Exception f) {
+			log.error("Error in ReadExcel: " + f.getMessage());
 		}
 
 	}
@@ -100,21 +215,24 @@ public class FileReaderServiceImpl implements FileReaderService {
 			Node baseNode = session.getNode(pathName);
 			if (baseNode != null) {
 				baseNode.setProperty(name, value);
+				log.info("Properties created at: " + baseNode + name + value);
 			}
 		} catch (RepositoryException e) {
-			log.error("RepositoryException in createProperties: "+e.getMessage());
+			log.error("RepositoryException in createProperties: " + e.getMessage());
 		}
-		
+
 	}
 
 	private void createNode(String pathName, Session session, String readCellData) {
 		try {
 			Node baseNode = session.getNode(pathName);
-			if(baseNode != null) {
-				baseNode = JcrUtils.getOrCreateByPath(baseNode, readCellData, false, AppConstants.NT_UNSTRUCTURED, AppConstants.NT_UNSTRUCTURED, true);
+			if (baseNode != null) {
+				baseNode = JcrUtils.getOrCreateByPath(baseNode, readCellData, false, AppConstants.NT_UNSTRUCTURED,
+						AppConstants.NT_UNSTRUCTURED, true);
+				log.info("Node created at: " + baseNode + readCellData);
 			}
 		} catch (RepositoryException e) {
-			log.error("RepositoryException in createNode: "+e.getMessage());
+			log.error("RepositoryException in createNode: " + e.getMessage());
 		}
 
 	}
@@ -122,21 +240,25 @@ public class FileReaderServiceImpl implements FileReaderService {
 	private String readCellData(Cell cell) {
 		String value = null;
 		try {
-			switch (cell.getCellTypeEnum()) {
-			case STRING:
-				value = cell.getStringCellValue();
-				break;
-			case NUMERIC:
-				value = String.valueOf(cell.getNumericCellValue());
-				break;
-			case BOOLEAN:
-				value = String.valueOf(cell.getBooleanCellValue());
-				break;
-			default:
-				break;
+			if (cell != null) {
+				switch (cell.getCellTypeEnum()) {
+				case BLANK:
+					break;
+				case STRING:
+					value = cell.getStringCellValue();
+					break;
+				case NUMERIC:
+					value = String.valueOf(cell.getNumericCellValue());
+					break;
+				case BOOLEAN:
+					value = String.valueOf(cell.getBooleanCellValue());
+					break;
+				default:
+					break;
+				}
 			}
 		} catch (Exception e) {
-			log.error("Exception in readCellData: "+e.getMessage());
+			log.error("Exception in readCellData: " + e.getMessage());
 		}
 		return value;
 	}
