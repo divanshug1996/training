4adeb5f96de2989047d92cd23c4f50320c034bbc
Jineet <61728875+vorajineet@users.noreply.github.com>
2020-10-12 23:24:44 +0530

    Update AppConstants.java

--- ProcessSpreadSheet - AEM6.5/core/src/main/java/org/corteva/aem/calc/core/constants/AppConstants.java
+++ ProcessSpreadSheet - AEM6.5/core/src/main/java/org/corteva/aem/calc/core/constants/AppConstants.java
@@ -4,10 +4,18 @@ public final class AppConstants {
 
 	public static final String EMPTY_STRING = "";
 	public static final String NT_UNSTRUCTURED = "nt:unstructured";
-	public static final String INITIAL_STRUCTURE = "/content/tc-calc/data/select-savings/";
+	public static final String INITIAL_STRUCTURE = "/content/tc-calc/data/";
 	public static final String SHEET_SELECT_SAVINGS = "Select Savings";
+	public static final String SHEET_ENLIST_AHEAD = "Enlist Ahead";
+	
 	public static final String SELECT_SAVINGS_REQUIRES_PREPAY = "Requires Prepay";
 	public static final String SELECT_SAVINGS_TRADE_CODE = "Trade Code";
 	public static final String SELECT_SAVINGS_SOLD_AS_UOM = "Sold as UOM";
 	public static final String SELECT_SAVINGS_EARNINGS_RATE = "Earnings Rate";
+	
+	public static final String ENLIST_AHEAD_PROGRAM_MATCH_RATE = "Program Match Rate";
+	public static final String ENLIST_AHEAD_APPLICATION_UOM = "Application UOM";
+	public static final String ENLIST_AHEAD_SOLD_AS_UOM = "Sold as UOM";
+	public static final String ENLIST_AHEAD_CONVERSION_FACTOR = "Conversion Factor";
+	public static final String ENLIST_AHEAD_PAY_RATE = "Pay Rate";
 }
